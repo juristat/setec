b9afa59777c375477710a293e37212f28f840618
Robert Ward <robert@rtward.com>
2019-01-04 09:28:33 -0600

    add a configurable logger

--- index.js
+++ index.js
@@ -3,94 +3,104 @@ const AWS = require('aws-sdk');
 const _ = require('lodash');
 const fs = require('fs');
 
-async function loadSecret(awsConfig, setecConfig, secretName) {
-  const ssm = new AWS.SSM(awsConfig);
-
-  const prefix = _.get(setecConfig, 'prefix', '');
-  const fullSecretName = `${prefix}${secretName}`;
-
-  try {
-    const secretObject = await Promise.fromCallback(
-      cb => ssm.getParameter({ Name: fullSecretName, WithDecryption: true }, cb),
-    );
-
-    const secretValue = secretObject.Parameter.Value;
-
-    return secretValue;
-  } catch (error) {
-    console.log(`error resolving secret: ${fullSecretName}`);
-    console.dir(error);
-
-    process.exit(1);
-
-    // This is just to avoid a linter error
-    return null;
-  }
-}
-
-function loadSecrets(awsConfig, setecConfig, config) {
-  if (_.isObject(config)) {
-    if (_.has(config, 'secret') && Object.keys(config).length === 1) {
-      return loadSecret(awsConfig, setecConfig, config.secret);
-    }
-
-    return Promise.props(_.mapValues(config, value => loadSecrets(awsConfig, setecConfig, value)));
-  }
-
-  if (_.isArray(config)) {
-    return Promise.map(config, value => loadSecrets(awsConfig, setecConfig, value));
-  }
-
-  return config;
-}
-
-function getConfig(configOrConfigfile) {
-  if (_.isString(configOrConfigfile)) {
-    const configFile = configOrConfigfile;
-    const ext = configFile.split('.').pop();
-    const hasExt = /\/[^/]+\.[^.]+$/.test(configFile);
-
-    /* eslint-disable global-require, import/no-dynamic-require */
-    if (!hasExt) throw new Error("Don't know what to do with a configFile with no extension");
-    else if (ext === 'json') return JSON.parse(fs.readFileSync(configFile));
-    else if (ext === 'js') return require(configFile);
-    else throw new Error(`Don't know what to do with a configFile ending in .${ext}`);
-    /* eslint-enable global-require, import/no-dynamic-require */
-  }
-
-  if (_.isObject(configOrConfigfile)) return configOrConfigfile;
-
-  throw new Error('Invalid config, must be either a string filename or object');
-}
-
-async function assumeRole({ role }) {
-  console.log(`assuming role ${role}`);
-
-  const sts = new AWS.STS();
-  const result = await Promise.fromCallback(
-    cb => sts.assumeRole({
-      RoleArn: role,
-      RoleSessionName: 'local-developer',
-    }, cb),
-  );
-
-  AWS.config.update({
-    accessKeyId: result.Credentials.AccessKeyId,
-    secretAccessKey: result.Credentials.SecretAccessKey,
-    sessionToken: result.Credentials.SessionToken,
-  });
-
-  console.log(`assumed role ${role}`);
-}
-
 class Setec {
   constructor(configOrConfigfile) {
-    this.config = getConfig(configOrConfigfile);
+    this.config = Setec.getConfig(configOrConfigfile);
 
     this.awsConfig = this.config.aws || {};
     AWS.config.update(this.awsConfig);
 
     this.setecConfig = this.config.setec || {};
+
+    /* eslint-disable no-console */
+    this.logger = this.config.logger || console.log;
+    /* eslint-enable no-console */
+  }
+
+  async loadSecret(awsConfig, setecConfig, secretName) {
+    const ssm = new AWS.SSM(awsConfig);
+
+    const prefix = _.get(setecConfig, 'prefix', '');
+    const fullSecretName = `${prefix}${secretName}`;
+
+    try {
+      const secretObject = await Promise.fromCallback(
+        cb => ssm.getParameter({ Name: fullSecretName, WithDecryption: true }, cb),
+      );
+
+      const secretValue = secretObject.Parameter.Value;
+
+      return secretValue;
+    } catch (error) {
+      this.logger(`error resolving secret: ${fullSecretName}`);
+      this.logger(JSON.stringify({
+        message: error.message,
+        stack: error.stack,
+      }));
+
+      process.exit(1);
+
+      // This is just to avoid a linter error
+      return null;
+    }
+  }
+
+  loadSecrets(awsConfig, setecConfig, config) {
+    if (_.isObject(config)) {
+      if (_.has(config, 'secret') && Object.keys(config).length === 1) {
+        return this.loadSecret(awsConfig, setecConfig, config.secret);
+      }
+
+      return Promise.props(
+        _.mapValues(config, value => this.loadSecrets(awsConfig, setecConfig, value)),
+      );
+    }
+
+    if (_.isArray(config)) {
+      return Promise.map(config, value => this.loadSecrets(awsConfig, setecConfig, value));
+    }
+
+    return config;
+  }
+
+  static getConfig(configOrConfigfile) {
+    if (_.isString(configOrConfigfile)) {
+      const configFile = configOrConfigfile;
+      const ext = configFile.split('.').pop();
+      const hasExt = /\/[^/]+\.[^.]+$/.test(configFile);
+
+      /* eslint-disable global-require, import/no-dynamic-require */
+      if (!hasExt) throw new Error("Don't know what to do with a configFile with no extension");
+      else if (ext === 'json') return JSON.parse(fs.readFileSync(configFile));
+      else if (ext === 'js') return require(configFile);
+      else throw new Error(`Don't know what to do with a configFile ending in .${ext}`);
+      /* eslint-enable global-require, import/no-dynamic-require */
+    }
+
+    if (_.isObject(configOrConfigfile)) return configOrConfigfile;
+
+    throw new Error('Invalid config, must be either a string filename or object');
+  }
+
+  async assumeRole({ role }) {
+    this.logger(`assuming role ${role}`);
+
+    const sts = new AWS.STS();
+    const result = await Promise.fromCallback(
+      cb => sts.assumeRole({
+        RoleArn: role,
+        RoleSessionName: 'local-developer',
+        DurationSeconds: 3600,
+      }, cb),
+    );
+
+    AWS.config.update({
+      accessKeyId: result.Credentials.AccessKeyId,
+      secretAccessKey: result.Credentials.SecretAccessKey,
+      sessionToken: result.Credentials.SessionToken,
+    });
+
+    this.logger(`assumed role ${role}`);
   }
 
   async load() {
@@ -98,13 +108,15 @@ class Setec {
 
     if (this.awsConfig.role) {
       if (this.config.production) {
-        throw new Error('programmatically assuming roles should only be used in non-production environments');
+        throw new Error(
+          'programmatically assuming roles should only be used in non-production environments',
+        );
       }
 
-      await assumeRole(this.awsConfig);
+      await this.assumeRole(this.awsConfig);
     }
 
-    const resolvedConfig = await loadSecrets(this.awsConfig, this.setecConfig, this.config);
+    const resolvedConfig = await this.loadSecrets(this.awsConfig, this.setecConfig, this.config);
     Object.assign(this.config, resolvedConfig);
     this.loaded = true;
 
